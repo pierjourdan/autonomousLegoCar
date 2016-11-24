95c20331304cca73fd93eb150525c11e69f947f8
pierjourdan <pier.jourdan@orange.com>
2016-11-24 11:15:23 +0100

    =arduino code, in progress...

--- Arduino/legoPowerControl.ino
+++ Arduino/legoPowerControl.ino
@@ -1,24 +1,17 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
+Control lego power motor and servo. They are connected via mtor shield.
+Receive a command from serial port (usb). The commande is a single character:
+receive: action:
++        increase motor speed
+-        decrease motor speed
+0        stop motor
+F        motor forward
+B        motor backward
+<        turn more left
+>        turn more right
+|        center direction
 */
 
-
-// the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -30,7 +23,7 @@ void loop() {
   if(Serial.available()) {  
     c = Serial.read();  
     Serial.print(c); 
-
+    //@todo: change to do the specified action
     if (c == '+') {
       blinkLed(1);
     }
